0c7131cd5ced2e94da39e4503149a340513f7e5a
Fillipe Sampaio <fillipessampaio@gmail.com>
2023-12-29 23:06:42 -0500

    Update main

--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,9 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.primaryContainer,
         ),
-        cardTheme: CardTheme().copyWith(
+        cardTheme: const CardTheme().copyWith(
           color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.all(8),
+          margin: const EdgeInsets.all(8),
         ),
         elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
